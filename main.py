4950b3bfe2645f56828dbcd6b5bb8ae6b522c939
cuteboysw <cuteboysw@gmail.com>
2022-11-16 10:04:50 +0900

    Validation_Batter data
    Validation_Batter_.csv is added.
    Validation_Pitcher_.csv is temporary data.

--- main.py
+++ main.py
@@ -1,29 +1,86 @@
 import pandas as pd
-from sklearn import svm
 from sklearn.linear_model import LinearRegression
 from sklearn import linear_model    #Lasso
+from sklearn.linear_model import SGDRegressor   #SGD
+from sklearn.pipeline import make_pipeline  #SGD
+from sklearn.preprocessing import StandardScaler    #SGD
+from sklearn.linear_model import Ridge  #Ridge
+
+print('\n===============BATTER=================')
 
 #data_url='.\dataset\Dataset_Batter.csv'
-
 csv = pd.read_csv('.\dataset\Dataset_Batter_.csv')
-#print(csv)
-testcsv=pd.read_csv('.\dataset\Test_Batter_.csv')
+valcsv=pd.read_csv('.\dataset\Validation_Batter_.csv')
 
-data2 = csv[["연도","생년월일","AVG","G","PA","AB","R","H","2B","3B","HR","TB","RBI","SAC","SF","BB","IBB","HBP","SO","GDP","SLG","OBP","OPS"]]
-label2 = csv["연봉(만원)"]
+data = csv[["연도","생년월일","AVG","AB","R","H","HR","RBI","BB","SO","OBP","OPS","RISP"]]
+label = csv["연봉(만원)"]
 
-testdata=testcsv[["연도","생년월일","AVG","G","PA","AB","R","H","2B","3B","HR","TB","RBI","SAC","SF","BB","IBB","HBP","SO","GDP","SLG","OBP","OPS"]]
+valdata=valcsv[["연도","생년월일","AVG","AB","R","H","HR","RBI","BB","SO","OBP","OPS","RISP"]]
 
+#SGD
+reg = make_pipeline(StandardScaler(),
+                    SGDRegressor(max_iter=1000, tol=1e-3))
+reg.fit(data, label)
+result=reg.predict(valdata)
+print('\nSGD')
+print(result)
+
+#LinearRegression
 lr = LinearRegression()
-lr.fit(data2,label2)
-result = lr.predict(testdata)
-print('LinearRegression')
+lr.fit(data,label)
+result = lr.predict(valdata)
+print('\nLinearRegression')
 print(result)
 
-clf = linear_model.Lasso(alpha=1.0)
-clf.fit(data2, label2)
-result = clf.predict(testdata)
-print('Lasso')
+#Lasso
+clfbl = linear_model.Lasso(alpha=1.0)
+clfbl.fit(data, label)
+result = clfbl.predict(valdata)
+print('\nLasso')
 print(result)
 
-#"이름",,"생년월일"
+#Ridge
+clfbr=linear_model.Ridge(alpha=1.0)
+clfbr.fit(data, label)
+result=clfbr.predict(valdata)
+print('\nRidge')
+print(result)
+
+print('\n===================PITCHER=================')
+
+csvp = pd.read_csv('.\dataset\Dataset_Pitcher_.csv')
+valcsvp=pd.read_csv('.\dataset\Vaildation_Pitcher_.csv')
+
+datap = csvp[["연도","생년월일","ERA","G","W","L","SV","HLD","WPCT","H","HR","BB","HBP","SO","R","ER","WHIP"]]
+labelp = csvp["연봉(만원)"]
+
+valdatap=valcsvp[["연도","생년월일","ERA","G","W","L","SV","HLD","WPCT","H","HR","BB","HBP","SO","R","ER","WHIP"]]
+
+#SGD
+regp = make_pipeline(StandardScaler(),
+                    SGDRegressor(max_iter=1000, tol=1e-3))
+regp.fit(datap, labelp)
+result=regp.predict(valdatap)
+print('\nSGD')
+print(result)
+
+#LinearRegression
+lrp = LinearRegression()
+lrp.fit(datap,labelp)
+result = lrp.predict(valdatap)
+print('\nLinearRegression')
+print(result)
+
+#Lasso
+clfpl = linear_model.Lasso(alpha=1.0)
+clfpl.fit(datap, labelp)
+result = clfpl.predict(valdatap)
+print('\nLasso')
+print(result)
+
+#Ridge
+clfpr=linear_model.Ridge(alpha=1.0)
+clfpr.fit(datap, labelp)
+result=clfpr.predict(valdatap)
+print('\nRidge')
+print(result)
