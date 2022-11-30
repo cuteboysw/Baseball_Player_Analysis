d774f02d1aa0b89f8b1b56ef448901f94ee9c225
cuteboysw <cuteboysw@gmail.com>
2022-11-30 10:32:41 +0900

    main.py modified

--- main.py
+++ main.py
@@ -24,11 +24,11 @@ try:
 except OSError:
     print ('Error whlie making a directory for result')
 
-datapath=input('타자에 대한 Test data가 있는 경로를 입력하세요.(상대 경로도 괜찮습니다.) 기본 경로(.\\dataset\\TestData_Batter_csv)로 하시려면 알파벳 소문자 n을 입력하세요.\n')
+datapath=input('타자에 대한 Test data가 있는 경로를 입력하세요.(상대 경로도 괜찮습니다.) 기본 경로(.\\dataset\\TestData_Batter_.csv)로 하시려면 알파벳 소문자 n을 입력하세요.\n')
 if(datapath=='n'):
     datapath='.\\dataset\\TestData_Batter_.csv'
 bat_path=pathlib.Path(datapath)
-datapath=input('타자에 대한 Test data가 있는 경로를 입력하세요.(상대 경로도 괜찮습니다.) 기본 경로(.\\dataset\\TestData_Pitcher_csv)로 하시려면 알파벳 소문자 n을 입력하세요.\n')
+datapath=input('투수에 대한 Test data가 있는 경로를 입력하세요.(상대 경로도 괜찮습니다.) 기본 경로(.\\dataset\\TestData_Pitcher_.csv)로 하시려면 알파벳 소문자 n을 입력하세요.\n')
 if(datapath=='n'):
     datapath='.\\dataset\\TestData_Pitcher_.csv'
 pit_path=pathlib.Path(datapath)
@@ -47,8 +47,8 @@ predset_pitcher=predict_pitcher #predict set of pitcher
 
 #LinearRegression
 regression=apply_LinearRegression
-regression.batter(bat_path)
-regression.pitcher(pit_path)
+predict_batter=regression.batter(bat_path)
+predict_pitcher=regression.pitcher(pit_path)
 df=pd.DataFrame({'예상연봉': predict_batter}).to_csv('.\\result\\LinearRegression_batter.csv')
 df=pd.DataFrame({'예상연봉': predict_pitcher}).to_csv('.\\result\\LinearRegression_pitcher.csv')
 predset_batter=np.concatenate((predset_batter, predict_batter), axis=0)
@@ -56,8 +56,8 @@ predset_pitcher=np.concatenate((predset_pitcher, predict_pitcher), axis=0)
 
 #Lasso
 regression=apply_Lasso
-regression.batter(bat_path)
-regression.pitcher(pit_path)
+predict_batter=regression.batter(bat_path)
+predict_pitcher=regression.pitcher(pit_path)
 df=pd.DataFrame({'예상연봉': predict_batter}).to_csv('.\\result\\Lasso_batter.csv')
 df=pd.DataFrame({'예상연봉': predict_pitcher}).to_csv('.\\result\\Lasso_pitcher.csv')
 predset_batter=np.concatenate((predset_batter, predict_batter), axis=0)
@@ -65,8 +65,8 @@ predset_pitcher=np.concatenate((predset_pitcher, predict_pitcher), axis=0)
 
 #Ridge
 regression=apply_Ridge
-regression.batter(bat_path)
-regression.pitcher(pit_path)
+predict_batter=regression.batter(bat_path)
+predict_pitcher=regression.pitcher(pit_path)
 df=pd.DataFrame({'예상연봉': predict_batter}).to_csv('.\\result\\Ridge_batter.csv')
 df=pd.DataFrame({'예상연봉': predict_pitcher}).to_csv('.\\result\\Ridge_pitcher.csv')
 predset_batter=np.concatenate((predset_batter, predict_batter), axis=0)
@@ -74,8 +74,8 @@ predset_pitcher=np.concatenate((predset_pitcher, predict_pitcher), axis=0)
 
 #Logistic Regression
 regression=apply_LogisticRegression
-regression.batter(bat_path)
-regression.pitcher(pit_path)
+predict_batter=regression.batter(bat_path)
+predict_pitcher=regression.pitcher(pit_path)
 df=pd.DataFrame({'예상연봉': predict_batter}).to_csv('.\\result\\LogisticRegression_batter.csv')
 df=pd.DataFrame({'예상연봉': predict_pitcher}).to_csv('.\\result\\LogisticRegression_pitcher.csv')
 predset_batter=np.concatenate((predset_batter, predict_batter), axis=0)
@@ -83,8 +83,8 @@ predset_pitcher=np.concatenate((predset_pitcher, predict_pitcher), axis=0)
 
 #k-neighbors_regression
 regression=apply_k_neighbors_regression
-regression.batter(bat_path)
-regression.pitcher(pit_path)
+predict_batter=regression.batter(bat_path)
+predict_pitcher=regression.pitcher(pit_path)
 df=pd.DataFrame({'예상연봉': predict_batter}).to_csv('.\\result\\k_neighbors_regression_batter.csv')
 df=pd.DataFrame({'예상연봉': predict_pitcher}).to_csv('.\\result\\k_neighbors_regression_pitcher.csv')
 predset_batter=np.concatenate((predset_batter, predict_batter), axis=0)
@@ -92,8 +92,8 @@ predset_pitcher=np.concatenate((predset_pitcher, predict_pitcher), axis=0)
 
 #Decision Tree
 regression=apply_decisionTree
-regression.batter(bat_path)
-regression.pitcher(pit_path)
+predict_batter=regression.batter(bat_path)
+predict_pitcher=regression.pitcher(pit_path)
 df=pd.DataFrame({'예상연봉': predict_batter}).to_csv('.\\result\\decisionTree_batter.csv')
 df=pd.DataFrame({'예상연봉': predict_pitcher}).to_csv('.\\result\\decisionTree_pitcher.csv')
 predset_batter=np.concatenate((predset_batter, predict_batter), axis=0)
@@ -101,15 +101,16 @@ predset_pitcher=np.concatenate((predset_pitcher, predict_pitcher), axis=0)
 
 #Get Result
 alg_list=['SGD', 'Linear Regression', 'Lasso', 'Ridge', 'Logistic Regression', 'k-neighbors_regression', 'Decision Tree']   #Regression Algorithm List
+batcnt=len(batcsv)
+pitcnt=len(pitcsv)
 predset_batter=predset_batter.reshape(-1, 7)
 predset_pitcher=predset_pitcher.reshape(-1, 7)
 df=pd.DataFrame(predset_batter, columns=alg_list).to_csv('.\\result\\total_prediction_batter.csv')
 df=pd.DataFrame(predset_pitcher, columns=alg_list).to_csv('.\\result\\total_prediction_pitcher.csv')
 print('전체 예측 결과가 result 폴더 내에 저장되었습니다.')
-print('잠시 후 예측 결과 그래프가 별도의 창에 표시됩니다.')
-csvcnt=len(batcsv)
-bat_index=np.arange(0, csvcnt)
+print('잠시 후 예측 결과 그래프가 별도의 창에 표시되며, Data_visualization/result_visaul에 저장됩니다.')
+bat_index=np.arange(0, batcnt)
 apply_pyplot.run(bat_index, predset_batter.T, 'batter', alg_list)
-csvcnt=len(pitcsv)
-pit_index=np.arange(0, csvcnt)
+pit_index=np.arange(0, pitcnt)
 apply_pyplot.run(pit_index, predset_pitcher.T, 'pitcher', alg_list)
+print('프로그램을 종료합니다.')
